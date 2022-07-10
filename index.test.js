438e14a62a189e0c63a6e83972e209af2c81d1e6
ElenaMughi <elena.mughi@gmail.com>
2022-07-10 17:57:16 +0300

    Try1

---
+++ index.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+
+describe("Netology page tests", () => {
+  beforeEach(() => {
+    page = browser.newPage();
+  });
+
+  afterEach( () => {
+    page.close();
+  });
+
+  test("test", async (page) => {
+
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
+
+    await page.click('li > [data-seance-id="129"]');
+    await expect(page).toHaveURL("http://qamid.tmweb.ru/client/hall.php");
+
+    await page.click("div:nth-child(7) > span:nth-child(1)");
+
+    await page.click('[class="acceptin-button"]');
+    await expect(page).toHaveURL("http://qamid.tmweb.ru/client/payment.php");
+
+    await page.click('[class="acceptin-button"]');
+    await expect(page).toHaveURL("http://qamid.tmweb.ru/client/ticket.php");
+
+    const title = await page.$eval("h2", (link) => link.textContent);
+    console.log(title);
+    expect(title).toEqual("Электронный билет");
+  });
+});
